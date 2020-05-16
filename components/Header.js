af8ac42f148f7535be02186881d50b216d2862a2
kamiada528@gmail.com <adrianna.kaminska@bbc.co.uk>
2020-05-17 00:43:33 +0100

    header and trigger added

---
+++ components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {View, Text, StyleSheet} from "react-native";
+import DrawerTrigger from "./DrawerTrigger";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <View style={styles.header}>
+        <DrawerTrigger />
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  header: {
+    paddingTop: 40,
+    backgroundColor: "whitesmoke",
+  },
+});
+
+export default Header;
--- screens/Recipes1.js
+++ screens/Recipes1.js
@@ -1,20 +1,24 @@
 import React from "react";
 import {Text, View, Button} from "react-native";
 import styles from "../styles.js";
+import Header from "../components/Header";
 
 export default class Recipes1 extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Button
-          title="Back to home"
-          onPress={() => this.props.navigation.navigate("Home")}
-        />
-        <Button
-          title="Go to screen 2"
-          onPress={() => this.props.navigation.navigate("Recipes_2")}
-        />
-      </View>
+      <React.Fragment>
+        <Header />
+        <View style={styles.container}>
+          <Button
+            title="Back to home"
+            onPress={() => this.props.navigation.navigate("Home")}
+          />
+          <Button
+            title="Go to screen 2"
+            onPress={() => this.props.navigation.navigate("Recipes_2")}
+          />
+        </View>
+      </React.Fragment>
     );
   }
 }
