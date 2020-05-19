e387d0eb7162d0175eb113358a34cccc40b87965
kamiada528@gmail.com <adrianna.kaminska@bbc.co.uk>
2020-05-19 12:24:30 +0100

    jumping onto making pagination happen

--- screens/Recipes1.js
+++ screens/Recipes1.js
@@ -1,43 +1,92 @@
 import React from "react";
-import {TouchableOpacity, View, Button, Text} from "react-native";
+import {
+  TouchableOpacity,
+  View,
+  Button,
+  Text,
+  ScrollView,
+  SafeAreaView,
+} from "react-native";
 import styles from "../styles.js";
 import Header from "../components/Header";
-import TitleHeader from "../components/TitleHeader";
+import Pagination from "../components/Pagination";
 import VisualRep from "../components/VisualRep";
 import RecipeContainer from "../components/RecipeContainer";
 import IngredientsContainer from "../components/IngredientsContainers";
 import {Ionicons} from "@expo/vector-icons";
-const data = require("../exampleData.json");
+import data from "../testData.json";
 
+//find current, prev and next
 
-//function to unpack array
-
-
-
+//display only current
 
 export default class Recipes1 extends React.Component {
   render() {
     return (
+      // <React.Fragment>
+      //   {data.map((recipes) => {
+      //     return (
+      //       <React.Fragment>
+      //     <SafeAreaView>
+      // <ScrollView>
+      //         <Header />
+      //         <View style={styles.container}>
+      //           <Pagination title={recipes.recipes.name} />
+      //           <VisualRep />
+      //           <IngredientsContainer />
+      //           <RecipeContainer />
+      //           <View>
+      //             <TouchableOpacity
+      //               onPress={() => this.props.navigation.navigate("Home")}
+      //             >
+      //               <Ionicons
+      //                 name={"md-arrow-round-back"}
+      //                 size={47}
+      //                 color={"grey"}
+      //               />
+      //               <Text>Go back</Text>
+      //             </TouchableOpacity>
+      //           </View>
+      //           <Button
+      //             title="Go to screen 2"
+      //             onPress={() => this.props.navigation.navigate("Recipes_2")}
+      //           />
+      //         </View>
+      //         </ScrollView>
+      // </SafeAreaView>
+      //       </React.Fragment>
+      //     );
+      //   })}
+      // </React.Fragment>
+
       <React.Fragment>
-        <Header />
-        <View style={styles.container}>
-          <TitleHeader title={"none"} />
-          <VisualRep />
-          <IngredientsContainer />
-          <RecipeContainer />
-          <View>
-            <TouchableOpacity
-              onPress={() => this.props.navigation.navigate("Home")}
-            >
-              <Ionicons name={"md-arrow-round-back"} size={47} color={"grey"} />
-              <Text>Go back</Text>
-            </TouchableOpacity>
-          </View>
-          <Button
-            title="Go to screen 2"
-            onPress={() => this.props.navigation.navigate("Recipes_2")}
-          />
-        </View>
+        <SafeAreaView>
+          <ScrollView>
+            <Header />
+            <View style={styles.container}>
+              <Pagination title={data.recipes.name} />
+              <VisualRep />
+              <IngredientsContainer ingredients={data.recipes.ingredients} />
+              <RecipeContainer recipe={data.recipes.recipe} />
+              <View>
+                <TouchableOpacity
+                  onPress={() => this.props.navigation.navigate("Home")}
+                >
+                  <Ionicons
+                    name={"md-arrow-round-back"}
+                    size={47}
+                    color={"grey"}
+                  />
+                  <Text>Go back</Text>
+                </TouchableOpacity>
+              </View>
+              <Button
+                title="Go to screen 2"
+                onPress={() => this.props.navigation.navigate("Recipes_2")}
+              />
+            </View>
+          </ScrollView>
+        </SafeAreaView>
       </React.Fragment>
     );
   }
